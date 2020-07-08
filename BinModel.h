ccfa56d45dc04bf78b893cabbd197e719356474b
christine-0729 <68034737+christine-0729@users.noreply.github.com>
2020-07-08 23:34:02 +0100

    Binomial Tree Pricing Model

---
+++ BinModel.h
@@ -0,0 +1,24 @@
+#ifndef BinModel_h
+#define BinModel_h
+
+class BinModel
+{
+    private:
+        double S0;
+        double U;
+        double D;
+        double R;
+
+    public:
+        double RiskNeutProb(); 
+
+        //computing the stock price at node n,i
+        double S(int n, int i);
+
+        //inputting, displaying and  checking model  data
+        int GetInputData();
+
+        double GetR();
+};
+
+#endif 
---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include "BinLattice.h"
+#include "BinModel.h"
+#include "Options.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    BinModel Model;
+
+    if (Model.GetInputData() == 1) return 1;
+
+    Put Option;
+    Option.GetInputData();
+    BinLattice<double> PriceTree;
+    BinLattice<bool> StoppingTree;
+    Option.PriceBySnell(Model, PriceTree, StoppingTree);
+    cout << "American put prices: " << endl << endl;
+    PriceTree.Display();
+    cout << "American put exercise policy: " << endl << endl;
+    StoppingTree.Display();
+    return 0;
+}
